98fcecdd11864feacd0f1ee8d6ac6c80d56e7de5
Andrew Denta <andrew@denta.co>
2022-11-11 14:27:36 -0500

    have link work

--- src/call-to-action-block.tsx
+++ src/call-to-action-block.tsx
@@ -5,19 +5,31 @@ import {
   Box,
   Link,
   useColorModeValue as mode,
+  LinkOverlay,
+  LinkBox,
 } from "@chakra-ui/react";
 
 const CallToActionBlock = () => {
   return (
     <Box>
       <Stack direction={{ base: "column", md: "row" }} spacing="4" mt="8">
-        <Button size="lg" minW="210px" colorScheme="blue" height="14" px="8">
-          Buy Now
-        </Button>
+        <LinkBox>
+          <LinkOverlay href="https://square.link/u/zDbsZF0z">
+            <Button
+              size="lg"
+              minW="210px"
+              colorScheme="blue"
+              height="14"
+              px="8"
+            >
+              Buy Now
+            </Button>
+          </LinkOverlay>
+        </LinkBox>
       </Stack>
       <Text mt="8" color={mode("gray.600", "gray.400")}>
         Already have an account?{" "}
-        <Link href="#" textDecoration="underline">
+        <Link href="https://app.whiskerverse.com" textDecoration="underline">
           Log in
         </Link>
       </Text>
